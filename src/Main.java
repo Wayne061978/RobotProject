762b9a095fe22247cecc12fe6975694270314493
wayne hunte <hunte7879@gmail.com>
2024-11-15 12:03:34 -0500

    first pack

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Robot myRobot = new Robot(); // Create a new Robot object
-        for (int i = 0; i < 15; i++)
-            myRobot.move(); // Make the robot move
+        myRobot.move(); // Make the robot move
         myRobot.checkBatteryLevel(); // Check battery level
         myRobot.stop(); // Stop the robot
         myRobot.chargeBattery(); // Charge the robot's battery
-        myRobot.displayInfo(); // robot’s name, color, and weight
-
-
     }
-
-
 
 }
